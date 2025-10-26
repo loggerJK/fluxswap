6b7b3922bf2a317c827f8334591da9ab6281304e
loggerJK <jiwon7258@gmail.com>
2025-10-27 01:36:46 +0900

    .

--- train/script/train_faceswap_vgg_maskidLoss.sh
+++ train/script/train_faceswap_vgg_maskidLoss.sh
@@ -1,5 +1,5 @@
 # *[Specify the config file path and the GPU devices to use]
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # *[Specify the config file path]
 export OMINI_CONFIG='./train/config/faceswap_vgg_maskidLoss.yaml'
@@ -11,7 +11,7 @@ export WANDB_API_KEY='f9831e23517e27f7ecac9b54bc2cdcabb3af8c33'
 echo $OMINI_CONFIG
 export TOKENIZERS_PARALLELISM=true
 
-python -m pdb -m omini.train_flux.train_faceswap
+# python -m pdb -m omini.train_flux.train_faceswap
 # python -m omini.train_flux.train_faceswap
 
-# accelerate launch --main_process_port 29502 -m omini.train_flux.train_faceswap
+accelerate launch --main_process_port 29502 -m omini.train_flux.train_faceswap
